c04c3d1984e078db884e08dd73f6363c0c26d634
afnad <afnadca2@gmail.om>
2025-11-18 16:48:17 +0530

    feat(server): add error handling and serve static frontend build

--- server/index.ts
+++ server/index.ts
@@ -14,13 +14,41 @@ app.use(helmet());
 app.use(cors({ origin: "*" }));
 app.use(cookieParser());
 
-connectDB();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//database connection
+connectDB();
+
+//routes
 app.use("/", (req: Request, res: Response) => {
   res.json({ message: "hi" });
+});
+
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, "frontEnd", "dist")));
+app.use((req, res, next) => {
+  if (req.path.startsWith("/api")) {
+    // Pass to API 404 if it's an API route
+    return next();
+  }
+  res.sendFile(path.join(_dirname, "frontEnd", "dist", "index.html"));
+});
+
+// Catch-all route for undefined routes
+app.use("/api", (req: Request, res: Response) => {
+  res.status(404).sendFile(path.join(_dirname, "frontEnd", "dist", "404.html"));
+});
+
+// Error-handling middleware
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
 
 const port = process.env.PORT || 5000;
